f3e6f5671085fa25cb74cffe29f5498698a2d798
Pengyu Wang <pwang2@laurentian.ca>
2025-01-09 21:51:06 -0500

    Update RunUnitTests.java

---
+++ app/src/test/java/pengyuw007/wagemate/RunUnitTests.java
@@ -0,0 +1,27 @@
+package pengyuw007.wagemate;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import static org.junit.Assert.*;
+
+import junit.framework.TestSuite;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class RunUnitTests {
+
+    public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("Unit tests");
+//        suite.addTest(ObjectTests.suite());
+//        suite.addTest(BusinessTests.suite());
+//        suite.addTest(PersistenceTests.suite());
+        return suite;
+    }
+}
